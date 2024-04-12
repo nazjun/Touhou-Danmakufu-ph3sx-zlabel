f6c1f75e1abe360b1765183cad8f1c75b068b42e
Natashi <32347635+Natashi@users.noreply.github.com>
2024-04-12 21:17:15 +0700

    Add fallthrough notice

--- source/GcLib/directx/DxScript.cpp
+++ source/GcLib/directx/DxScript.cpp
@@ -6082,6 +6082,7 @@ gstd::value DxScript::Func_ObjFileB_ReadString(gstd::script_machine* machine, in
 				ByteOrder::Reverse(&res[0], strSize);
 		}
 		case CODE_ACP:
+			__fallthrough;
 		case CODE_UTF8:
 		default:
 		{
