8bc0bae1f6938a606d245afc0721bf877d73bae6
Nicholas Logan <63122321+nijlogan@users.noreply.github.com>
2026-03-08 18:47:00 -0400

    Update GstdConstant.hpp

--- source/GcLib/gstd/GstdConstant.hpp
+++ source/GcLib/gstd/GstdConstant.hpp
@@ -2,7 +2,7 @@
 #include "../pch.h"
 
 const std::wstring DNH_EXE_NAME = L"th_dnh_ph3sx-zlabel.exe";
-const std::wstring DNH_VERSION = L"v1.33a-pre";
+const std::wstring DNH_VERSION = L"v1.33a-pre2";
 
 constexpr const uint64_t _GAME_VERSION_RESERVED = /*e*/621;		//OWO!!!!!
 constexpr const uint64_t _GAME_VERSION_MAJOR = 1;
